adb4cfa053850f449f5066ecad1ee844b2165cb7
Anthony Eden <anthonyeden@gmail.com>
2010-12-14 13:01:59 +0100

    only set the DNSimple.url property if the local url value is not null.

--- dnsimple.js
+++ dnsimple.js
@@ -3,14 +3,13 @@ DNSimple = {
     if (typeof jQuery == 'undefined')
       throw("You must install JQuery to use the DNSimple JS API");
 
-    if (url == null)
-      url = 'https://dnsimple.com';
-    
     var headID = document.getElementsByTagName("head")[0];         
     var dnsimpleScript = document.createElement('script');
     dnsimpleScript.type = 'text/javascript';
     dnsimpleScript.onload = function() {
-      DNSimple.url = url;
+      if (url != null) {
+        DNSimple.url = url;
+      }
     };
     dnsimpleScript.src = url + '/javascripts/dnsimple-api.js';
     headID.appendChild(dnsimpleScript);
